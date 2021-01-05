c22902fbafe863b8efae4442b5f8ad39292a66f0
Raphael Suter <raphael.werner.suter@gmail.com>
2021-01-05 13:41:00 +0100

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(<h1>Hello World</h1>, document.getElementById("root"));
